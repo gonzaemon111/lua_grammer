b422129746f03f93e66a6c298b736aadeb739d36
gonzaemon111 <goki14914@icloud.com>
2019-09-07 00:59:33 +0900

    add variable.lua

---
+++ variable5.lua
@@ -0,0 +1,11 @@
+hoge = 10
+piyo = 20
+foo = 30.256
+
+print( string.format("hoge の値は%d です",hoge) )
+print( string.format("piyo の値は%d です",piyo) )
+print( string.format("値は%d です", 50) )
+print( string.format("hoge は%d piyo は%d です",hoge,piyo) )
+print( string.format("foo の値は%f です",foo) )
+print( string.format("hoge の値は%f です",hoge) )
+-- print( string.format("foo の値は%d です",foo) )
